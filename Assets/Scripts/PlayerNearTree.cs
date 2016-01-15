36d089f4111428ed9e8195177dd190424423a634
Sam <sam.fields94@gmail.com>
2016-01-14 20:28:56 -0800

    PlayerNearTree -> tree drops nut

---
+++ Assets/Scripts/PlayerNearTree.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerNearTree : Scenario {
+
+	public GameObject player;
+	public GameObject theTree;
+	List<GameObject> adjTrees;
+	bool isTriggered; 
+
+	// Use this for initialization
+	void Start () {
+		isTriggered = false;
+		player = GameObject.Find ("Player");
+		adjTrees = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void IsTriggered() {
+	}
+	
+	void Outcome(){
+	}
+
+	public void addTree(GameObject tree) {
+		adjTrees.Add (tree);
+	}
+
+	public void removeTree(GameObject tree) {
+		adjTrees.Remove (tree);
+	}
+}
