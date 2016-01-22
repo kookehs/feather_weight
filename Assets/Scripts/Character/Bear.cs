18ff5e901f8a9308a5c587866c969c045a344123
Sam <sam.fields94@gmail.com>
2016-01-21 19:36:00 -0800

    Beginnings of bear script

--- Assets/Scripts/Character/Bear.cs
+++ Assets/Scripts/Character/Bear.cs
@@ -1,7 +1,16 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
+public enum BearState {
+	UNAWARE,
+	HOSTILE,
+	FRIENDLY
+}
+
+[RequireComponent (typeof (CharacterController))]
 public class Bear : MonoBehaviour {
+
+	private BearState state = BearState.UNAWARE;
 
 	public GameObject player;
 	public GameObject scenarioController;
