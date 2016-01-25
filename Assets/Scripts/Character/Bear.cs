6c7810ea6f50150f3642fee75d64f9b6771cdcb8
Sam <sam.fields94@gmail.com>
2016-01-24 17:55:28 -0800

    Health script, progress on Bear AI

--- Assets/Scripts/Character/Bear.cs
+++ Assets/Scripts/Character/Bear.cs
@@ -18,16 +18,26 @@ public class Bear : MonoBehaviour
 	public GameObject target;
 	public bool isPlayerNear;
 	public float friendliness;
+	private Vector3 forward;
+	private Vector3 desiredAngle;
+	float turnTimer;
+	public float rotateBy = 420f;
+	float step;
+	CharacterController controller;
 
 	// Use this for initialization
 	void Start ()
 	{
-
+		forward = transform.forward;
+		desiredAngle = -forward;
 		player = GameObject.Find ("Player");
 		target = null;
 		isPlayerNear = false;
 		friendliness = 0f;
-	
+		turnTimer = 2f;
+		step = Time.deltaTime * 2f;
+		controller = GetComponent<CharacterController> ();
+
 	}
 	
 	// Update is called once per frame
@@ -36,9 +46,8 @@ public class Bear : MonoBehaviour
 		switch (state) {
 		case BearState.HOSTILE:
 			Debug.Log (">:(");
-			if (target == player) {
-
-			}
+			faceTarget (target);
+			moveToward (target);
 			if (friendliness > 0)
 				state = BearState.FRIENDLY; 
 			break;
@@ -49,14 +58,47 @@ public class Bear : MonoBehaviour
 			break;
 		case BearState.UNAWARE:
 			Debug.Log ("-___-");
+			turnTimer -= Time.deltaTime;
+			float randomNum = Random.Range (0, 100); 
+			//Debug.Log (turnTimer);
+			//	The turn timer triggers a new time being set, and a new desired angle
+			if (turnTimer <= 0f) {
+				turnTimer = Random.Range (2, 4);
+				desiredAngle = -desiredAngle;
+			}
+			//	I will appear to be looking around arbitrarily
+			Vector3 lookAround = Vector3.RotateTowards (transform.forward, desiredAngle, rotateBy * Mathf.Deg2Rad * Time.deltaTime, 1000);
+			if (lookAround != Vector3.zero)
+				transform.rotation = Quaternion.LookRotation (lookAround);
+
+			//Vector3 lookAround = Vector3.RotateTowards (transform.forward, -transform.forward, rotateBy * Mathf.Deg2Rad * Time.deltaTime, 1000);
+			//if (lookAround != Vector3.zero) transform.rotation = Quaternion.LookRotation(lookAround);
+
+			//	If the player goes near me I will change states
 			if (Vector3.Distance (player.transform.position, transform.position) < 5f) {
-				if (friendliness > 0) state = BearState.FRIENDLY;
-				if (friendliness <= 0) state = BearState.HOSTILE;
+				if (friendliness > 0) {
+					target = player;
+					state = BearState.FRIENDLY;
+				} else if (friendliness <= 0) {
+					target = player;
+					state = BearState.HOSTILE;
+				}
 			}
 			break;
 		}	
 	}
 
+	void faceTarget (GameObject target)
+	{
+		if (transform.position.x < target.transform.position.x) {
+			desiredAngle = forward;
+		} else
+			desiredAngle = -forward; 
+		Vector3 faceTarget = Vector3.RotateTowards (transform.forward, desiredAngle, rotateBy * Mathf.Deg2Rad * Time.deltaTime, 1000);
+		if (faceTarget != Vector3.zero)
+			transform.rotation = Quaternion.LookRotation (faceTarget);
+	}
+	
 	void OnTriggerEnter (Collider other)
 	{
 		if (other.gameObject.Equals (player)) {
@@ -79,5 +121,14 @@ public class Bear : MonoBehaviour
 	public void decreaseFriendliness ()
 	{
 		friendliness += 1;
+	}
+
+	private void moveToward(GameObject target){
+		Vector3 ignoreTargetY = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+		Vector3.MoveTowards(transform.position, ignoreTargetY, step);
+		if ((controller.collisionFlags & CollisionFlags.Below)==0)
+		{
+			controller.Move(Vector3.down * Time.deltaTime * 5);
+		}
 	}
 }
