733f2b3d1dbda0e165a64ef5123346643958dd25
Sam <sam.fields94@gmail.com>
2016-01-30 15:27:05 -0800

    First pass: Click to strike with sword

--- Assets/SwordController.cs
+++ Assets/SwordController.cs
@@ -4,21 +4,27 @@ using System.Collections;
 public class SwordController : MonoBehaviour {
 	
 	public GameObject mainChar;
+	public GameObject swordSpawn;
+	public GameObject mySword;
 
 	// Use this for initialization
 	void Start () {
 
 		mainChar = GameObject.Find ("Player");
+		swordSpawn = GameObject.Find ("SwordSpawn");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		//	When I click, spawn the sword at the position of the swordSpawn
 		if (Input.GetMouseButtonDown (0)) {
-
+			Debug.Log (transform.rotation);
+			Instantiate (mySword, swordSpawn.transform.position, transform.rotation);
 		}
 
+		//	Maintain the position of the swordSpawn
 		RaycastHit hit;
 		Vector3 whereHit = Vector3.zero;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
