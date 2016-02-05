1b71304a7c25690fb426233366fd6b66be39f027
Sam <sam.fields94@gmail.com>
2016-02-04 16:46:22 -0800

    Implemented debug counters for survival stats, moved some scripts around

---
+++ Assets/Scripts/Character/SurvivalGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SurvivalGUI : MonoBehaviour {
+
+	Text myText;
+	public GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text> ();
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		myText.text = "Water " + player.GetComponent<Hydration> ().hydration + "\nFood " + player.GetComponent<FoodLevel> ().foodLevel + "\nHealth " + player.GetComponent<Health> ().health;
+	}
+}
