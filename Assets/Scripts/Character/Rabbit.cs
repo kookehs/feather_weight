4ac0455fdf80d8ba137bdaec9b1a63b868fad4ef
alprusa <aprusa@ucsc.edu>
2016-02-26 16:33:44 -0800

    A killer bunny should drop antlers

--- Assets/Scripts/Character/Rabbit.cs
+++ Assets/Scripts/Character/Rabbit.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Rabbit : Animal {
 
 	public override void performStateCheck(){
+		bool killerRabbit = false;
 
 		//	If we are not running...
 		if (state != AnimalState.RUNNING) {
@@ -14,7 +15,22 @@ public class Rabbit : Animal {
 				state = AnimalState.RUNNING;
 				//	At a certain distance, it becomes unaware of the player.
 			}
-		} else {
+		} else if(state != AnimalState.HOSTILE){
+			if (killerRabbit) {
+				//transsform change sprite
+				//	If a rabbit is close enough to a player
+				if (Vector3.Distance (player.transform.position, transform.position) < 10f) {
+					//	It attacks the player
+					target = player;
+					state = AnimalState.HOSTILE;
+					//	At a certain distance, it becomes unaware of the player.
+				}
+
+				if (GetComponent<Health> ().isDead ()) {
+					DropAntler ();
+				}
+			}
+		}{
 			if (runTime < 150f) {
 				runTime += Time.deltaTime;
 			} else {
@@ -22,5 +38,9 @@ public class Rabbit : Animal {
 				state = AnimalState.UNAWARE;
 			}
 		}
+	}
+
+	public void DropAntler(){
+		Instantiate (Resources.Load("Antler"), new Vector3 (transform.position.x, transform.position.y + 10, transform.position.z), transform.rotation);
 	}
 }
