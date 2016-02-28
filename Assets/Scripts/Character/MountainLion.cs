36a01dc8059889079df84dbd0c79285f60994963
Sam <sam.fields94@gmail.com>
2016-02-28 14:20:35 -0800

    First pass Mountain Lion, trying to implement animals "jumping" between off mesh links

---
+++ Assets/Scripts/Character/MountainLion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MountainLion : Animal
+{
+
+	public override void performStateCheck ()
+	{
+
+		//	Consider the distance between player and mountain lion
+		if (Vector3.Distance (player.transform.position, transform.position) < 15f) {
+			Debug.Log ("Hostile");
+			target = player;
+			state = AnimalState.HOSTILE;
+		} else {
+			Debug.Log ("Unaware");
+			state = AnimalState.UNAWARE;
+		}
+
+	}
+}
