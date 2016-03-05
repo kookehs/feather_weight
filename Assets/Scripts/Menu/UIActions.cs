3f6449106e8e09a7013013b686b619843291ac99
alprusa <aprusa@ucsc.edu>
2016-03-05 04:08:09 -0800

    Start Menu

---
+++ Assets/Scripts/Menu/UIActions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIActions : MonoBehaviour {
+
+	public void StartGame(){
+		Application.LoadLevel ("final_world");
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
