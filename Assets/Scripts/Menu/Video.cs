376d65bb81b4ea4c280acbac805d313e2ae589db
alprusa <aprusa@ucsc.edu>
2016-03-06 02:27:29 -0800

    Game Over screen

---
+++ Assets/Scripts/Menu/Video.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//[RequireComponent (typeof(AudioSource))]
+public class Video : MonoBehaviour {
+
+	public MovieTexture movie;
+	//private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		//audio = GetComponent<AudioSource> ();
+		//audio.clip = movie.audioClip;
+		movie.loop = true;
+		movie.Play ();
+		//audio.Play ();
+	}
+}
