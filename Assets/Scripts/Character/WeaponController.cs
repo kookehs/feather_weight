b9526f1ddbe972625875655ac1444edd00c0e17e
Sam <sam.fields94@gmail.com>
2016-03-11 13:34:20 -0800

    Fixed switching weapons

--- Assets/Scripts/Character/WeaponController.cs
+++ Assets/Scripts/Character/WeaponController.cs
@@ -29,17 +29,12 @@ public class WeaponController : MonoBehaviour
 		spawnPosFront = GameObject.Find ("SpawnPosFront").transform.position;
 		spawnPosBack = GameObject.Find ("SpawnPosBack").transform.position;
 
-		//	The equipped weapon is instantiated at the spawn point, and then made a child of this object.
-		if (myWeapon.tag.StartsWith ("Spear") || myWeapon.tag.StartsWith ("Pick_Axe")) {
+		if (myWeapon != null) {
 			myWeapon = Instantiate (myWeapon, spawnPosFront, Quaternion.identity) as GameObject;
 			myWeapon.transform.parent = GameObject.Find ("SpawnPosFront").transform;
-		} else if (myWeapon.tag.StartsWith ("Sword") || myWeapon.tag.StartsWith ("Wood_Axe")) {
-			myWeapon = Instantiate (myWeapon, spawnPosBack, Quaternion.identity) as GameObject;
-			myWeapon.transform.parent = GameObject.Find ("SpawnPosBack").transform;
+			myWeapon.name = "EquipedWeapon";
+			myWeapon.layer = LayerMask.NameToLayer ("Default");
 		}
-
-		myWeapon.name = "EquipedWeapon";
-		myWeapon.layer = LayerMask.NameToLayer ("Default");
 
 		//	An AudioSource is declared here in code
 		buzz = GetComponent<AudioSource> ();
@@ -50,7 +45,8 @@ public class WeaponController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (player == null) return;
+		if (player == null)
+			return;
 
 		if (myWeapon.tag.StartsWith ("Spear") || myWeapon.tag.StartsWith ("Pick_Axe")) {
 			if (Input.GetMouseButtonDown (0) && coolingDown == false && !player.GetComponent<PlayerMovementRB> ().mouseHovering) {
@@ -66,6 +62,31 @@ public class WeaponController : MonoBehaviour
 				if (Time.time - cooldownTime >= .5f)
 					coolingDown = false;
 			}
+			//	
+			//	The following code maintains the position of the SpawnPos object,
+			//	which floats around the player at a fixed distance and at an angle
+			//	that depends on where the mouse cursor is.
+			//
+			//	Declaration of ray, hit, and whereHit
+			if (!coolingDown) {
+				RaycastHit hit;
+				Vector3 whereHit = Vector3.zero;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+				//	Ray debug statement
+				Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
+
+				//	A ray is cast from the mouse position. The y of the hit position
+				//	is replaced with the y of the player position.
+				if (Physics.Raycast (ray, out hit)) {
+					whereHit = hit.point;
+					whereHit.y = player.transform.position.y;
+				}
+
+				//	The rotation of the SpawnPos is determined based on the ray
+				targetDirection = whereHit - player.transform.position;
+				transform.rotation = Quaternion.LookRotation (targetDirection);
+			}
 		} else if (myWeapon.tag.StartsWith ("Sword") || myWeapon.tag.StartsWith ("Wood_Axe")) {
 			if (Input.GetMouseButtonDown (0) && coolingDown == false && !player.GetComponent<PlayerMovementRB> ().mouseHovering) {
 				myWeapon.SetActive (true);
@@ -78,52 +99,47 @@ public class WeaponController : MonoBehaviour
 			//Deal with cooldown
 			if (coolingDown == true) {
 
-				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation (-targetDirection), Time.deltaTime * 1000);
+				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation (targetDirection), Time.deltaTime * 1000);
 				if (Time.time - cooldownTime >= .5f)
 					coolingDown = false;
 			}
-		}
-		//	
-		//	The following code maintains the position of the SpawnPos object,
-		//	which floats around the player at a fixed distance and at an angle
-		//	that depends on where the mouse cursor is.
-		//
-		//	Declaration of ray, hit, and whereHit
-		if (!coolingDown) {
-			RaycastHit hit;
-			Vector3 whereHit = Vector3.zero;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			//	
+			//	The following code maintains the position of the SpawnPos object,
+			//	which floats around the player at a fixed distance and at an angle
+			//	that depends on where the mouse cursor is.
+			//
+			//	Declaration of ray, hit, and whereHit
+			if (!coolingDown) {
+				RaycastHit hit;
+				Vector3 whereHit = Vector3.zero;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
-			//	Ray debug statement
-			Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
+				//	Ray debug statement
+				Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
 
-			//	A ray is cast from the mouse position. The y of the hit position
-			//	is replaced with the y of the player position.
-			if (Physics.Raycast (ray, out hit)) {
-				whereHit = hit.point;
-				whereHit.y = player.transform.position.y;
+				//	A ray is cast from the mouse position. The y of the hit position
+				//	is replaced with the y of the player position.
+				if (Physics.Raycast (ray, out hit)) {
+					whereHit = hit.point;
+					whereHit.y = player.transform.position.y;
+				}
+
+				//	The rotation of the SpawnPos is determined based on the ray
+				targetDirection = whereHit - player.transform.position;
+				transform.rotation = Quaternion.LookRotation (-targetDirection);
 			}
-
-			//	The rotation of the SpawnPos is determined based on the ray
-			targetDirection = whereHit - player.transform.position;
-			transform.rotation = Quaternion.LookRotation (targetDirection);
 		}
+
 	}
 
-	public void equipWeapon(ref GameObject newWeapon) {
+	public void equipWeapon (ref GameObject newWeapon)
+	{
 
-		//	The equipped weapon is instantiated at the spawn point, and then made a child of this object.
-		if (newWeapon.tag.StartsWith ("Spear") || newWeapon.tag.StartsWith ("Pick_Axe")) {
-			//myWeapon = Instantiate (newWeapon, spawnPosFront, Quaternion.identity) as GameObject;
-			newWeapon.transform.position = spawnPosFront;
-			newWeapon.gameObject.SetActive (true);
-			newWeapon.transform.parent = GameObject.Find ("SpawnPosFront").transform;
-		} else if (newWeapon.tag.StartsWith ("Sword") || newWeapon.tag.StartsWith ("Wood_Axe")) {
-			//myWeapon = Instantiate (newWeapon, spawnPosBack, Quaternion.identity) as GameObject;
-			newWeapon.transform.position = spawnPosBack;
-			newWeapon.gameObject.SetActive (true);
-			newWeapon.transform.parent = GameObject.Find ("SpawnPosBack").transform;
-		}
+		GameObject spawnPosFront = GameObject.Find ("SpawnPosFront");
+
+		newWeapon.transform.position = spawnPosFront.transform.position;
+		newWeapon.gameObject.SetActive (true);
+		newWeapon.transform.parent = spawnPosFront.transform;
 
 		myWeapon = newWeapon;
 		myWeapon.name = "EquipedWeapon";
