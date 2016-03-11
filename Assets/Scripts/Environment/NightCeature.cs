be4e959e802dded35237b67191233723b22c2ca0
alprusa <aprusa@ucsc.edu>
2016-03-11 02:25:06 -0800

    Wolves and rabbits added and rabbit animation done and bear cubs are little bears now

--- Assets/Scripts/Environment/NightCeature.cs
+++ Assets/Scripts/Environment/NightCeature.cs
@@ -5,22 +5,23 @@ public class NightCeature : MonoBehaviour {
 
 	public GameObject weatherController;
 	public string creatureTagName;
+	public string dayTime;
 
-	private GameObject[] wolves;
+	private GameObject[] creatures;
 
 	void Start(){
-		wolves = GameObject.FindGameObjectsWithTag (creatureTagName);
+		creatures = GameObject.FindGameObjectsWithTag (creatureTagName);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (weatherController.GetComponent<WeatherController> ().GetTimeOfDay ().Equals ("NIGHT")) {
-			for (int i = 0; i < wolves.Length; i++) {
-				wolves [i].SetActive (true);
+		if (weatherController.GetComponent<WeatherController> ().GetTimeOfDay ().Equals (dayTime)) {
+			for (int i = 0; i < creatures.Length; i++) {
+				creatures [i].SetActive (true);
 			}
 		} else {
-			for (int i = 0; i < wolves.Length; i++) {
-				wolves [i].SetActive (false);
+			for (int i = 0; i < creatures.Length; i++) {
+				creatures [i].SetActive (false);
 			}
 		}
 	}
