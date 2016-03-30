7296ef407fc58b08acf3de3200f33194548b7db7
Kookehs <kookehmonsters@yahoo.com>
2016-03-30 16:24:07 -0700

    Wolf fix

--- Assets/Scripts/Environment/NightCeature.cs
+++ Assets/Scripts/Environment/NightCeature.cs
@@ -12,21 +12,24 @@ public class NightCeature : MonoBehaviour {
 	void Start(){
 		creatures = GameObject.FindGameObjectsWithTag (creatureTagName);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (weatherController.GetComponent<WeatherController> ().GetTimeOfDay ().Equals ("DAY")) {
+		if (weatherController.GetComponent<WeatherController> ().GetTimeOfDay ().Equals (dayTime)) {
 			for (int i = 0; i < creatures.Length; i++) {
 				if(creatures[i] != null)
 					creatures [i].SetActive (true);
 			}
-			weatherController.GetComponent<WorldContainer> ().NeutralBunnies ();
 		} else {
 			for (int i = 0; i < creatures.Length; i++) {
 				if (creatures [i] != null && !creatureTagName.Equals ("Rabbit"))
 					creatures [i].SetActive (false);
 			}
-			weatherController.GetComponent<WorldContainer>().KillerBunnies();
 		}
-	}
+                if (weatherController.GetComponent<WeatherController> ().GetTimeOfDay ().Equals ("DAY")) {
+                        weatherController.GetComponent<WorldContainer> ().NeutralBunnies ();
+                } else {
+                        weatherController.GetComponent<WorldContainer>().KillerBunnies();
+                }
+        }
 }
