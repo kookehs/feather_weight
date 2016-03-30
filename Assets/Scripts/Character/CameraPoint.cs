e5bbeead7a7ee78892d0fc16f7bbe1d405e83bd0
Tai Trinh <trinhbtai@gmail.com>
2016-03-30 00:19:47 -0700

    Camera rotation using RMB

--- Assets/Scripts/Character/CameraPoint.cs
+++ Assets/Scripts/Character/CameraPoint.cs
@@ -3,16 +3,10 @@ using System.Collections;
 
 public class CameraPoint : MonoBehaviour {
 	public Transform target;            // The position that that camera will be following.
-	public float smoothing = 1f;        // The speed with which the camera will be following.
 	private WorldContainer the_world;
 	private PlayerMovementRB player;
-	Vector3 offset;                     // The initial offset from the target.
 
-	/*void Awake () {
-		transform.position = Camera.main.transform.position;
-		transform.rotation = Camera.main.transform.rotation;
-		offset = transform.position - target.position;
-	}*/
+	private float rotate_speed = 75f;
 
 	void Start ()
 	{
@@ -21,25 +15,9 @@ public class CameraPoint : MonoBehaviour {
 	}
 
 	void Update() {
-		if (Input.GetKey ("e"))
-			SmoothRotateCamera (45);
-		else if (Input.GetKey ("q"))
-			SmoothRotateCamera (-45);
-		/*if (target) {
-			if (NoMovementInput () && !player.isMoving ()) { 
-				if (Input.GetKey ("e"))
-					SmoothRotateCamera (90);
-				else if (Input.GetKey ("q"))
-					SmoothRotateCamera (-90);
-			}
-			Vector3 targetCamPos = target.position + offset;
-			//transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
-			transform.position = targetCamPos;
-		}*/
-	}
-
-	private bool NoMovementInput() {
-		return !(Input.GetKey ("w") || Input.GetKey ("a") || Input.GetKey ("s") || Input.GetKey ("d"));
+		if (Input.GetMouseButton (1)) {
+			SmoothRotateCamera(Input.GetAxis("Mouse X") * rotate_speed);
+		}
 	}
 
 	private void SmoothRotateCamera (float angle) {
