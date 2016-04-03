9f4c4b36a3ec0947a1dc110802e30d08d643b7d5
tbtrinh <trinhbtai@gmail.com>
2016-04-03 09:49:12 -0700

    made is_bridge_point a public bool for water points

--- Assets/Scripts/Environment/DistancePoints_Water.cs
+++ Assets/Scripts/Environment/DistancePoints_Water.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class DistancePoints_Water : DistancePoints
 {
-	private bool _is_bridge_point = false;
-
-	public bool is_bridge_point {
-		get { return _is_bridge_point;  }
-		set { _is_bridge_point = value; }
-	}
+	public bool is_bridge_point = false;
 }
 
