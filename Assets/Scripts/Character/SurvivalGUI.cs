2fdf44d8a765ccf4c8814a6606e1b6d421d7d5ec
alprusa <aprusa@ucsc.edu>
2016-04-04 22:34:51 -0700

    Suvival stats using icons and fake day night cycle hud display

--- Assets/Scripts/Character/SurvivalGUI.cs
+++ Assets/Scripts/Character/SurvivalGUI.cs
@@ -4,18 +4,44 @@ using UnityEngine.UI;
 
 public class SurvivalGUI : MonoBehaviour {
 
-	Text myText;
+	Image icon;
+	private float initalValue;
 	public GameObject player;
 	
 	// Use this for initialization
 	void Start () {
-		myText = GetComponent<Text> ();
+		icon = GetComponent<Image> ();
 		player = GameObject.Find ("Player");
+		initalValue = player.GetComponent<Health> ().health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		myText.text = "Water " + player.GetComponent<Hydration> ().hydration + "\nFood " + player.GetComponent<FoodLevel> ().foodLevel + "\nHealth " + player.GetComponent<Health> ().health;
+		switch(icon.name){
+			case "HealthOverlay":
+				float curHealth = player.GetComponent<Health> ().health / initalValue; //this is to convert the value to be 0-1 for the fillAmound value
+				if(player.GetComponent<Health> ().health.Equals(initalValue)) curHealth = 1;
+				if (player.GetComponent<Health> ().health == 0) curHealth = 0;
+
+				icon.fillAmount = curHealth;
+				break;
+			case "HungerOverlay":
+				float curHunger = player.GetComponent<FoodLevel> ().foodLevel / initalValue; //this is to convert the value to be 0-1 for the fillAmound value
+				if(player.GetComponent<Health> ().health.Equals(initalValue)) curHunger = 1;
+				if (player.GetComponent<Health> ().health == 0) curHunger = 0;
+
+				icon.fillAmount = curHunger;
+				break;
+			case "ThirstOverlay":
+				float curHydration = player.GetComponent<Hydration> ().hydration / initalValue; //this is to convert the value to be 0-1 for the fillAmound value
+				if(player.GetComponent<Health> ().health.Equals(initalValue)) curHydration = 1;
+				if (player.GetComponent<Health> ().health == 0) curHydration = 0;
+
+				icon.fillAmount = curHydration;
+				break;
+		}
+		
+
+		//myText.text = "Water " + player.GetComponent<Hydration> ().hydration + "\nFood " + player.GetComponent<FoodLevel> ().foodLevel + "\nHealth " + player.GetComponent<Health> ().health;
 	}
 }
