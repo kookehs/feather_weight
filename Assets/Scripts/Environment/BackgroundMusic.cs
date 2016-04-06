6b1039f976482ccca36359f69a41a9c876c5033b
alprusa <aprusa@ucsc.edu>
2016-04-06 06:56:15 -0700

    Moved player added new overworld audio track

---
+++ Assets/Scripts/Environment/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	public AudioSource overworld;
+	//AudioSource ambient;
+
+	// Use this for initialization
+	void Start () {
+		overworld.Pause ();
+		StartCoroutine ("DelayOverWorld");
+	}
+	
+	IEnumerable DelayOverWorld(){
+		yield return new WaitForSeconds (0.5f);
+		Debug.Log ("cor");
+		overworld.Play ();
+	}
+}
