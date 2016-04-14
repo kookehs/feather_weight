8feb62c1151f34d58e522fa57464d188a30d1e2d
alprusa <aprusa@ucsc.edu>
2016-04-14 16:13:45 -0700

    3D assets added inclueds fence, cage, and portal

---
+++ Assets/Scripts/Quest/PortalVid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalVid : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+		movie.loop = true;
+		movie.Play ();
+
+		GetComponent<Material> ().mainTexture = movie as MovieTexture;
+	}
+}
