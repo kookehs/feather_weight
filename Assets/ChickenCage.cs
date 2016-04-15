8eef19afa184efc8d5039b3a28dca9bfbed070a9
Sam <sam.fields94@gmail.com>
2016-04-14 18:55:04 -0700

    ChickenCage prefab and script created to handle room win conditions

---
+++ Assets/ChickenCage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickenCage : MonoBehaviour {
+
+	public GameObject forfeitChicken;
+
+	public AudioSource squawk;
+	public GameObject feathers;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.tag.Equals("Bear")) {
+			squawk.Play ();
+			Instantiate (feathers, transform.position, Quaternion.identity);
+			Destroy (forfeitChicken);
+			Destroy (gameObject, squawk.clip.length);
+		}
+	}
+}
