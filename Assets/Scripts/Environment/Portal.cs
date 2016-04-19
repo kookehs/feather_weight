26d6da5ce07b9b082f2b23a46dcb0e39bcbaf552
Kookehs <kookehmonsters@yahoo.com>
2016-04-19 15:42:27 -0700

    Added portal script

---
+++ Assets/Scripts/Environment/Portal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+    public string next_level;
+
+    private void
+    OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            Application.LoadLevel(next_level);
+        }
+    }
+}
