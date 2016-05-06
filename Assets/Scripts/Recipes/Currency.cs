f97bdeca70a0db4be1c35f094e9dd2d0aa603068
alprusa <aprusa@ucsc.edu>
2016-05-06 16:14:43 -0700

    Weapon stays equiped

--- Assets/Scripts/Recipes/Currency.cs
+++ Assets/Scripts/Recipes/Currency.cs
@@ -19,7 +19,7 @@ public class Currency : MonoBehaviour {
 		//this inner code needs to be used in the wave when the chickenRoom is being opened up from the shop
 		if (Input.GetKeyUp (KeyCode.Backspace)) {
 			GameObject.Find ("PlayerUIElements").GetComponent<GrabPlayerUIElements> ().RestPlayerUI ();
-			GameObject.Find ("InventoryContainer").GetComponent<InventoryController> ().moveGameObjectsParent ();
+			//GameObject.Find ("InventoryContainer").GetComponent<InventoryController> ().moveGameObjectsParent ();
 			try{
 				GameObject.Find ("PlayerUICurrent").transform.FindChild("EventSystem").gameObject.SetActive(true);
 			}catch(Exception e){
@@ -31,20 +31,10 @@ public class Currency : MonoBehaviour {
 
 		//this inner code needs to be used in the wave when the shop is being opened up
 		if (Input.GetKeyUp (KeyCode.KeypadEnter)) {
-			InventoryController inventory = GameObject.Find ("InventoryContainer").GetComponent<InventoryController> ();
 			try{
 				GameObject.Find ("PlayerUICurrent").transform.FindChild("EventSystem").gameObject.SetActive(false);
 			}catch(Exception e){
 				Debug.Log ("No EventSystem" + e.Message);
-			}
-
-			for (int i = 0; i < inventory.inventoryItems.Count; i ++){
-				GameObject itemHave = inventory.inventoryItems[i];
-
-				if (itemHave.name == "EquipedWeapon") {
-					inventory.currentlySelected = i;
-					inventory.UseEquip ();
-				}
 			}
 
 			Application.LoadLevel ("ShopCenter");
