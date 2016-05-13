adade3a22466ecf2997d9d3c5bcfdaaef280d709
tbtrinh <trinhbtai@gmail.com>
2016-05-13 13:20:08 -0700

    Twitch Action Controller provides feedback

--- Assets/Scripts/Environment/Destroyable.cs
+++ Assets/Scripts/Environment/Destroyable.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Destroyable : Strikeable {
+public class Destroyable : Strikeable
+{
 
 	public GameObject collectable;
 
@@ -10,40 +11,45 @@ public class Destroyable : Strikeable {
 
 	protected char[] separator = { '_' };
 
-        private GameObject _chicken;
-        private bool _has_chicken = false;
+	private GameObject _chicken;
+	private bool _has_chicken = false;
 
-        public GameObject chicken {
-            get {return this._chicken;}
-            set {this._chicken = value;}
-        }
+	public GameObject chicken {
+		get { return this._chicken; }
+		set { this._chicken = value; }
+	}
 
-        public bool has_chicken {
-            get {return this._has_chicken;}
-            set {this._has_chicken = value;}
-        }
+	public bool has_chicken {
+		get { return this._has_chicken; }
+		set { this._has_chicken = value; }
+	}
 
-        public void
-        HideChicken(GameObject chicken) {
-            _chicken = chicken;
-            _chicken.SetActive(false);
-        }
+	public void
+        HideChicken (GameObject chicken)
+	{
+		_chicken = chicken;
+		_has_chicken = true;
+		_chicken.SetActive (false);
+	}
 
-        public void
-        ReleaseChicken() {
-            if (_chicken != null) {
-                _chicken.SetActive(true);
-            }
-        }
+	public void
+        ReleaseChicken ()
+	{
+		if (_chicken != null) {
+			_has_chicken = false;
+			_chicken.SetActive (true);
+		}
+	}
 
-	protected override bool AfterHit(string hitter) {
+	protected override bool AfterHit (string hitter)
+	{
 		invincible = false;
 		Health health = GetComponent<Health> ();
 		// DropCollectable (hitter);
 		if (health != null && health.IsDead ()) {
-                        ReleaseChicken();
+			ReleaseChicken ();
 			Destroy (gameObject);
-                }
+		}
 		return false;
 	}
 }
