d8a3f562833231789f15beebeb9002adc10a1e79
Sam Fields <sam.fields94@gmail.com>
2016-05-18 13:02:10 -0700

    Fixed particle effects reference

--- Assets/Scripts/Character/NPCs/Chicken.cs
+++ Assets/Scripts/Character/NPCs/Chicken.cs
@@ -319,7 +319,7 @@ public class Chicken : Animal
 
 	public void Pop ()
 	{
-		Instantiate (Resources.Load ("FeatherPop"), transform.position, Quaternion.identity);
+		Instantiate (Resources.Load ("Particle Effects/FeatherPop"), transform.position, Quaternion.identity);
 		WorldContainer.Remove (gameObject);
 	}
 
