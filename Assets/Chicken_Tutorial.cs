74bb152e4bfb2c80f07223c399fe3697784d09a9
Sam <sam.fields94@gmail.com>
2016-05-27 15:41:22 -0700

    Tutorial chicken created and placed in scene

---
+++ Assets/Chicken_Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chicken_Tutorial : Chicken {
+
+	private bool beenHit = false;
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+		gameObject.name = "HIT ME";
+	}
+	
+	public override IEnumerator WaitAndEnableCollection ()
+	{
+		if (beenHit == false) {
+			Destroy(transform.FindChild ("Mouse").gameObject);
+			beenHit = true;
+		}
+		gameObject.name = "COLLECT ME";
+		return base.WaitAndEnableCollection ();
+	}
+
+	public override void DisableCollection ()
+	{
+		base.DisableCollection ();
+		gameObject.name = "HIT ME";
+	}
+
+
+}
